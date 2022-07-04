c714c4465aa883cb6a63c7a993987c44efb3269b
Smarajit Das <sdas9696969@gmail.com>
2022-07-04 23:00:52 +0530

    Update

--- lib/ui/home_screen.dart
+++ lib/ui/home_screen.dart
@@ -17,58 +17,63 @@ class _HomeScreenState extends State<HomeScreen> {
   changeCurrentIndex(index) => setState(() => _currentIndex = index);
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-          currentIndex: _currentIndex,
-          onTap: (index) => changeCurrentIndex(index),
-          selectedFontSize: 12,
-          showUnselectedLabels: false,
-          backgroundColor: appBackgroundColor,
-          selectedItemColor: appTextColor,
-          unselectedItemColor: appTextColor,
-          showSelectedLabels: false,
-          items: List.generate(
-              bottomNavBarIcons.length,
-              (index) => BottomNavigationBarItem(
-                  backgroundColor: appBackgroundColor,
-                  label: bottomNavBarIcons[index].label,
-                  icon: NuemorphicIcon(icon: bottomNavBarIcons[index].icon)))),
-      body: CustomScrollView(slivers: <Widget>[
-        SliverAppBar(
-            stretch: true,
-            expandedHeight: 200,
-            centerTitle: true,
-            automaticallyImplyLeading: false,
-            title: ListTile(
-                trailing: const CircleAvatar(),
-                leading:
-                    IconButton(onPressed: () {}, icon: const Icon(Icons.menu))),
-            bottom: AppBar(
-                automaticallyImplyLeading: false,
-                title: ListTile(
-                    title: const Text('Hello $userName',
-                        style: TextStyle(
-                            fontSize: 36, fontWeight: FontWeight.w700)),
-                    subtitle: const Text('Welcome Back!'),
-                    trailing: IconButton(
-                        onPressed: () {}, icon: const Icon(Icons.settings))))),
-        const SliverToBoxAdapter(child: SizedBox(height: 36)),
-        SliverFillRemaining(
-            child: Wrap(
-                runAlignment: WrapAlignment.center,
-                crossAxisAlignment: WrapCrossAlignment.start,
-                alignment: WrapAlignment.center,
-                children: List.generate(
-                    cards.length,
-                    (index) => GestureDetector(
-                        onTap: () => Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const SalesRevenue())),
-                        child: NuemorphicCard(
-                            index: index,
-                            value: cards[index].value,
-                            headingIcon: cards[index].icon,
-                            title: cards[index].title)))))
-      ]));
+  Widget build(BuildContext context) {
+    BottomNavigationBar bottomNavigationBar = BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => changeCurrentIndex(index),
+        selectedFontSize: 12,
+        showUnselectedLabels: false,
+        backgroundColor: appBackgroundColor,
+        selectedItemColor: appTextColor,
+        unselectedItemColor: appTextColor,
+        showSelectedLabels: false,
+        items: List.generate(
+            bottomNavBarIcons.length,
+            (index) => BottomNavigationBarItem(
+                backgroundColor: appBackgroundColor,
+                label: bottomNavBarIcons[index].label,
+                icon: NuemorphicIcon(icon: bottomNavBarIcons[index].icon))));
+
+    return Scaffold(
+        bottomNavigationBar: bottomNavigationBar,
+        body: CustomScrollView(slivers: <Widget>[
+          SliverAppBar(
+              stretch: true,
+              expandedHeight: 200,
+              centerTitle: true,
+              automaticallyImplyLeading: false,
+              title: ListTile(
+                  trailing: const CircleAvatar(),
+                  leading: IconButton(
+                      onPressed: () {}, icon: const Icon(Icons.menu))),
+              bottom: AppBar(
+                  automaticallyImplyLeading: false,
+                  title: ListTile(
+                      title: const Text('Hello $userName',
+                          style: TextStyle(
+                              fontSize: 36, fontWeight: FontWeight.w700)),
+                      subtitle: const Text('Welcome Back!'),
+                      trailing: IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.settings))))),
+          const SliverToBoxAdapter(child: SizedBox(height: 36)),
+          SliverFillRemaining(
+              child: Wrap(
+                  runAlignment: WrapAlignment.center,
+                  crossAxisAlignment: WrapCrossAlignment.start,
+                  alignment: WrapAlignment.center,
+                  children: List.generate(
+                      cards.length,
+                      (index) => GestureDetector(
+                          onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => const SalesRevenue())),
+                          child: NuemorphicCard(
+                              index: index,
+                              value: cards[index].value,
+                              headingIcon: cards[index].icon,
+                              title: cards[index].title)))))
+        ]));
+  }
 }
